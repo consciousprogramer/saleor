3dd36ee0fc15274f3c1ea70d65b1139c9ca3aa1d
IKarbowiak <iga.karbowiak@mirumee.com>
2021-06-11 11:54:26 +0200

    Update resolvers for query voucher and sale

--- saleor/graphql/discount/resolvers.py
+++ saleor/graphql/discount/resolvers.py
@@ -1,10 +1,15 @@
 from ...core.tracing import traced_resolver
 from ...discount import models
-from ..channel import ChannelQsContext
+from ..channel import ChannelContext, ChannelQsContext
 from ..utils.filters import filter_by_query_param
 
 VOUCHER_SEARCH_FIELDS = ("name", "code")
 SALE_SEARCH_FIELDS = ("name", "value", "type")
+
+
+def resolve_voucher(id, channel):
+    sale = models.Voucher.objects.filter(id=id).first()
+    return ChannelContext(node=sale, channel_slug=channel) if sale else None
 
 
 @traced_resolver
@@ -14,6 +19,11 @@ def resolve_vouchers(info, query, channel_slug, **_kwargs) -> ChannelQsContext:
     if channel_slug:
         qs = qs.filter(channel_listings__channel__slug=channel_slug)
     return ChannelQsContext(qs=qs, channel_slug=channel_slug)
+
+
+def resolve_sale(id, channel):
+    sale = models.Sale.objects.filter(id=id).first()
+    return ChannelContext(node=sale, channel_slug=channel) if sale else None
 
 
 @traced_resolver
