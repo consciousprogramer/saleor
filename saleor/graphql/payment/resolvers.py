7fbe429cfc02de4ad16cde770c309cf3ce653fed
IKarbowiak <iga.karbowiak@mirumee.com>
2021-06-11 09:36:32 +0200

    Update resolver for query by payment

--- saleor/graphql/payment/resolvers.py
+++ saleor/graphql/payment/resolvers.py
@@ -5,6 +5,10 @@ from ..utils.filters import filter_by_query_param
 PAYMENT_SEARCH_FIELDS = ["id"]
 
 
+def resolve_payment_by_id(id):
+    return models.Payment.objects.filter(id=id).first()
+
+
 @traced_resolver
 def resolve_payments(info, query):
     queryset = models.Payment.objects.all().distinct()
